e2f0ca0f4bd4c655bbafeef8adeeaae2c70c3998
on-pacman <97858381+on-pacman@users.noreply.github.com>
2022-01-17 21:34:47 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+#update your termux
+pkg updaate
+
+#install fish
+pkg install fish
+
+#fish being the main shell
+chsh ~/../usr/bin/bash
+
+#prerequisites
+pkg install ruby
+gem install lolcat
+pkg install exa
+pkg install curl
+
+#install omf plugin fish shell
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
+fish install --path=~/.local/share/omf --config=~/.config/omf
+#install plugin
+omf update
+omf install sashimi
+
+
+#move config.fish
+cp config.fish ~/.config/fish/
+
+#finish
+exit
